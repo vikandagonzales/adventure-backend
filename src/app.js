06ca79ba5a321f79f1ba30b9c6d1b521dad19345
vikandagonzales <vikanda.gonzales@gmail.com>
2018-12-21 10:37:10 -0800

    set up knexfile.js and app.js

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').load();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// TOOLS
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// USER ROUTES
+// app.use('/guests', require('./routes/guests'));
+
+// DEFAULT ROUTE
+app.use((req, res, next) => {
+  next({status: 404, message: 'Route not found'});
+});
+
+// ERROR HANDLING
+app.use((err, req, res, next) => {
+  const errorMessage = {};
+  if (process.env.NODE_ENV !== 'production' && err.stack) {
+    errorMessage.stack = err.stack;
+    errorMessage.status = err.status || 500;
+    errorMessage.message = err.message || 'Internal Server Error';
+    res.status(errorMessage.status).send(errorMessage);
+  }
+});
+
+// SERVER
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
