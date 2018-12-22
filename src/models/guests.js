7c525a1d146d99cc576e9543ce02b49011b23a9b
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-22 11:50:31 -0800

    changed create guest so it only allows creation when group limit has not been reached

--- src/models/guests.js
+++ src/models/guests.js
@@ -1,5 +1,5 @@
 const db = require('../../db');
-const groups = require('./groups');
+const groupsModel = require('./groups');
 
 const getAll = () => {
   return db('guests');
@@ -12,14 +12,19 @@ const getOne = id => {
 };
 
 const create = async ({group_id, first_name, last_name}) => {
-  const guests = await getAll();
-  console.log(guests);
-  // return db('guests')
-  //   .insert({group_id, first_name, last_name})
-  //   .returning('*')
-  //   .then(([data]) => {
-  //     return data;
-  //   });
+  const allGuests = await getAll();
+  const guests = await allGuests.filter(guest => guest.group_id == group_id);
+  const group = await groupsModel.getOne(group_id);
+  if (guests.length < group.limit) {
+    return db('guests')
+      .insert({group_id, first_name, last_name})
+      .returning('*')
+      .then(([data]) => {
+        return data;
+      });
+  } else {
+    throw {status: 400, message: 'Limit for group exceeded'};
+  }
 };
 
 const update = (id, {accepted}) => {
