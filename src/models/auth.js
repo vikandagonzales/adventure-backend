73cd81f3eea0648ef38baa20bd9c63ff87c85edc
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-24 15:44:58 -0800

    made plus one guests unable to log in

--- src/models/auth.js
+++ src/models/auth.js
@@ -5,6 +5,7 @@ const login = ({first_name, last_name}) => {
   return guestsModel.getOneByName(first_name, last_name)
     .then(guest => {
       if (!guest) throw {status: 400, message: 'Guest not found'};
+      if (guest.plus_one) throw {status: 400, message: 'Only invited guests can log in'};
       return guest;
     });
 };
