1af56d55e32fbbe7e9db540c7127ed347386958b
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-23 23:09:41 -0800

    added migrations for details

---
+++ db/migrations/20181221171953_details.js
@@ -0,0 +1,18 @@
+const TABLE_NAME = 'details';
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(TABLE_NAME, table => {
+    table.string('mother').notNullable();
+    table.string('father').notNullable();
+    table.string('host').notNullable();
+    table.string('date').notNullable();
+    table.string('time').notNullable();
+    table.string('location').notNullable();
+    table.string('registry').notNullable();
+    table.string('registry_link').notNullable();
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTableIfExists(TABLE_NAME);
+};
