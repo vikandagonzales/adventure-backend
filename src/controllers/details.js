706839560cc184fa88193e949d30c6b24451584e
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-12-28 23:09:23 -0800

    removed registry keys from details routes

--- src/controllers/details.js
+++ src/controllers/details.js
@@ -11,11 +11,12 @@ const update = (req, res, next) => {
     !req.body.mother &&
     !req.body.father &&
     !req.body.host &&
+    !req.body.host_contact &&
     !req.body.date &&
     !req.body.time &&
     !req.body.location &&
-    !req.body.registry &&
-    !req.body.registry_link
+    !req.body.map &&
+    !req.body.rsvp_date
   ) return next({status: 400, message: 'No changes to make'});
   model.update(req.body)
     .then(data => res.status(200).send({data}))
--- src/models/details.js
+++ src/models/details.js
@@ -5,16 +5,17 @@ const get = () => {
     .first();
 };
 
-const update = ({mother, father, host, date, time, location, registry, registry_link}) => {
+const update = ({mother, father, host, host_contact, date, time, location, map, rsvp_date}) => {
   const updated = {};
   mother? updated.mother = mother : null;
   father ? updated.father = father : null;
   host ? updated.host = host : null;
+  host_contact ? updated.host_contact = host_contact : null;
   date ? updated.date = date : null;
   time ? updated.time = time : null;
   location ? updated.location = location : null;
-  registry ? updated.registry = registry : null;
-  registry_link ? updated.registry_link = registry_link : null;
+  map ? updated.map = map : null;
+  rsvp_date ? updated.rsvp_date = rsvp_date : null;
   return db('details')
     .update(updated)
     .returning('*')
